ce23d69cb06b86089638817ba1520c446e2cd502
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-06-20 13:29:19 +0800

    duped archive check

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,14 @@
 class Article < ApplicationRecord
+  VALID_STATUSES = %w[public private archived]
+
   validates :title, presence: true
   validates :author, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+  validates :status, inclusion: { in: VALID_STATUSES }
 
   has_many :comments
+
+  def archived?
+    status == 'archived'
+  end
 end
