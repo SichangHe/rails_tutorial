a2bf58642ac9af790e2fc9804b328c3d9aa665e5
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-06-20 13:19:03 +0800

    db:migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_620_032_013) do
+ActiveRecord::Schema[7.0].define(version: 20_220_620_051_627) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.string 'author'
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'status'
   end
 
   create_table 'comments', force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_620_032_013) do
     t.integer 'article_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'status'
     t.index ['article_id'], name: 'index_comments_on_article_id'
   end
 
